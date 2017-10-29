4f636780b1d631817673d0574c59206f1ee9b088
Vladimir <originalblack@mail.ru>
2017-10-30 01:05:08 +0300

    create script's skeleton

--- week2/week02_01.py
+++ week2/week02_01.py
@@ -1,3 +1,22 @@
-import sys
+import os
+import tempfile
+import argparse
+import json
+
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
+
+def add_value(user_key, user_value):
+    pass
+
+def get_value(user_key):
+    pass
+
+parser = argparse.ArgumentParser(description='Simple key-value store script')
+parser.add_argument("--key", help="set key to adding value to storage, if --key is single argument, then return data or None")
+parser.add_argument("--value", help="set value to storing to storage")
+args = parser.parse_args()
 
 
+data = 'parse arg'
+with open(storage_path, 'w') as f:
+    json.dump(data, f, ensure_ascii=False)
