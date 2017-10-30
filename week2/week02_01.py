1797e041e5d446cce5f56b397a7114d908f313a2
Vladimir <originalblack@mail.ru>
2017-10-31 01:20:37 +0300

    add test method

--- week2/week02_01.py
+++ week2/week02_01.py
@@ -5,18 +5,39 @@ import json
 
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
-def add_value(user_key, user_value):
+
+def check_key_exsist(user_key, json_data):
+    dic_data = json.loads(json_data)
+    for element in dic_data:
+        if user_key in element:
+            return True
+    return False
+
+
+def add_value(user_key, user_value, json_data):
+    d = {}
+    d[user_key] = user_key
+
     pass
 
-def get_value(user_key):
-    pass
+def get_value(user_key, json_data):
+    if check_key_exsist(user_key, json_data):
+        ##  get values
+    return None
+
+
 
 parser = argparse.ArgumentParser(description='Simple key-value store script')
 parser.add_argument("--key", help="set key to adding value to storage, if --key is single argument, then return data or None")
 parser.add_argument("--value", help="set value to storing to storage")
 args = parser.parse_args()
+if args.key:
+    test_hash = {
+        "one" : "two",
+        "1" : "test",
+    }
+    print(test_hash[args.key])
 
 
-data = 'parse arg'
 with open(storage_path, 'w') as f:
-    json.dump(data, f, ensure_ascii=False)
+    d = json.loads(f)
