c686d5e6dcecda25c6430aaf480a818f847e987a
Vladimir <originalblack@mail.ru>
2017-11-01 01:05:00 +0300

    done get_key_value and set_key_value method

--- week2/week02_01.py
+++ week2/week02_01.py
@@ -6,43 +6,49 @@ import json
 storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
 
-def check_key_exsist(user_key, json_data):
-    dic_data = json.loads(json_data)
-    for element in dic_data:
-        if user_key in element:
-            return True
+def check_key_exsist(user_key, dict):
+    if user_key in dict:
+        return True
     return False
 
+def get_dict_from_json(json_data):
+    return json.loads(json_data)
+
+def extract_dict_to_json(dictinary):
+    return json.dumps(dictinary)
 
 def add_value(user_key, user_value, json_data):
-    d = {}
-    d[user_key] = user_key
+    d = get_dict_from_json(json_data)
+    if check_key_exsist(user_key, d):
+        return d[user_key].append(user_value)
+    d[user_key] = [user_value]
+    return extract_dict_to_json(d)
 
-    pass
 
 def get_value(user_key, json_data):
-    if check_key_exsist(user_key, json_data):
-        ##  get values
+    d = get_dict_from_json(json_data)
+    if check_key_exsist(user_key, d):
+        return ", ".join(map(str, d[user_key]))
     return None
 
 #  WORK WITH JSON!!!!!
 #data = {"1":"one", "2":"two"} # python словарь
 #json_data = json.dumps(data) # упаковываем
 #parsed_json = json.loads(json_data) #распаковываем
-#print data == parsed_json
+
 
 
 parser = argparse.ArgumentParser(description='Simple key-value store script')
-parser.add_argument("--key", help="set key to adding value to storage, if --key is single argument, then return data or None")
+parser.add_argument("--key", help="set key to adding value to storage, if --key is single argument, then return data or None", required=True)
 parser.add_argument("--value", help="set value to storing to storage")
 args = parser.parse_args()
-if args.key:
-    test_hash = {
-        "one" : "two",
-        "1" : "test",
+
+test_hash = {
+        "one" : ["two"],
+        "1" : ["test", "ddd"],
     }
-    print(test_hash[args.key])
+if args.key:
 
+    print(type(args.key))
+    print(get_value(args.key, extract_dict_to_json(test_hash)))
 
-with open(storage_path, 'w') as f:
-    d = json.loads(f)
