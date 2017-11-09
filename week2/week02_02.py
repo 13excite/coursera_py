3a637efdbb2445f281ca9f593401c3f3bf586c54
Vladimir <originalblack@mail.ru>
2017-11-09 23:35:44 +0300

    add test func

--- week2/week02_02.py
+++ week2/week02_02.py
@@ -1,5 +1,15 @@
 import functools
 
 def to_json(func):
-    @functools.wraps(func)
+    #@functools.wraps(func)
+
     pass
+
+
+
+def get_data():
+  return {
+    'data': 42
+  }
+
+print(get_data())
