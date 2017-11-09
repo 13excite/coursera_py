1994da1db221beee9badf503af356accbc2124bf
Vladimir <originalblack@mail.ru>
2017-11-09 23:47:34 +0300

    done task2

--- week2/week02_02.py
+++ week2/week02_02.py
@@ -1,16 +1,23 @@
+import json
 import functools
 
 def to_json(func):
     @functools.wraps(func)
     def json_decode(*args, **kwargs):
         result = func(*args, **kwargs)
+        if result  != 'null' or result != None:
+            json_data = json.dumps(result)
+            return json_data
+    return json_decode
 
 
 
 
+@to_json
 def get_data():
   return {
     'data': 42
   }
 
-print(type(get_data()))
+print(get_data())
+print(get_data.__name__)
