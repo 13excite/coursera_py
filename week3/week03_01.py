d886343af3cc83b0f2eb9f67de31a6a62068fe6f
Vladimir <originalblack@mail.ru>
2017-11-23 23:46:08 +0300

    up class

--- week3/week03_01.py
+++ week3/week03_01.py
@@ -4,7 +4,10 @@ import sys
 class FileReader:
     def __init__(self, file_path):
         self.file_path = file_path
-        pass
+
 
     def read(self, path):
+        with open(path) as f:
+            f.read()
+            f.close()
         pass
