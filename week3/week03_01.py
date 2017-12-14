77820cfc4d9a141d579cedb90a6bca1c6e8b84c5
Vladimir <originalblack@mail.ru>
2017-12-14 23:04:42 +0300

    need debug

--- week3/week03_01.py
+++ week3/week03_01.py
@@ -2,12 +2,17 @@ import sys
 
 
 class FileReader:
+
     def __init__(self, file_path):
         self.file_path = file_path
 
+    def read(self):
+        try:
+            file = open(self.file_path, 'r')
+            return print(file.read())
+        except IOError:
+            return ""
 
-    def read(self, path):
-        with open(path) as f:
-            f.read()
-            f.close()
-        pass
+
+reader = FileReader("README.md")
+print(reader.read())
