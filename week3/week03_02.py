6c4bd82db89677320109947b964678d48b9d589e
Vladimir <originalblack@mail.ru>
2017-12-17 02:06:07 +0300

    done csv_data_extractor

--- week3/week03_02.py
+++ week3/week03_02.py
@@ -6,6 +6,9 @@ class CarBase:
     def __init__(self, brand, photo_file_name, carrying):
         pass
 
+    def get_photo_file_ext(self):
+        pass
+
 
 class Car(CarBase):
     def __init__(self, brand, photo_file_name, carrying, passenger_seats_count):
@@ -16,6 +19,9 @@ class Truck(CarBase):
     def __init__(self, brand, photo_file_name, carrying, body_whl):
         pass
 
+    def get_body_volume(self):
+        pass
+
 
 class SpecMachine(CarBase):
     def __init__(self, brand, photo_file_name, carrying, extra):
@@ -24,4 +30,13 @@ class SpecMachine(CarBase):
 
 def get_car_list(csv_filename):
     car_list = []
-    return car_list
+    reader = csv.reader(csv_filename)
+    for row in reader:
+        if row:
+            car_list.append(row[0].strip().split(';'))
+    return car_list[1:]
+
+with open('_af3947bf3a1ba3333b0c891e7a8536fc_coursera_week3_cars.csv', 'r') as f:
+    data = get_car_list(f)
+    print(data)
+    print(len(data[0]))
