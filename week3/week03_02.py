6dafd7a250ee2d43f2890d8341c256e8c09e44cf
Vladimir <originalblack@mail.ru>
2017-12-19 00:13:40 +0300

    refactoring get_car_list function

--- week3/week03_02.py
+++ week3/week03_02.py
@@ -14,8 +14,6 @@ class CarBase:
         if self.photo_file_name:
             return os.path.splitext(self.photo_file_name)[1]
 
-
-
 class Car(CarBase):
     def __init__(self, car_type, brand, photo_file_name, carrying, passenger_seats_count):
         super().__init__(car_type, brand, photo_file_name, carrying)
@@ -44,46 +42,31 @@ def get_car_list(csv_filename):
     try:
         with open(csv_filename,'r') as f:
             reader = csv.reader(f, delimiter=";")
+            next(reader)
+            i = 0
             for row in reader:
-               # print(row)
-                if row and row[0].replace(";",""):
-                    car_list.append(row)
+                if len(row) < 7:
+                    continue
+                if row[0] == 'car':
+                    car_list.append(1)
+                    car_list[i] = Car(row[0], row[1], row[3], row[5], row[2])
+                    i += 1
+                if row[0] == 'truck':
+                    car_list.append(1)
+                    car_list[i] = Truck(row[0], row[1], row[3], row[5], row[4])
+                    i += 1
+                if row[0] == 'spec_machine':
+                    car_list.append(1)
+                    car_list[i] = SpecMachine(row[0], row[1], row[3], row[5], row[6])
+                    i += 1
     except IOError:
         return "Error opening file"
-    return car_list[1:]
+    return car_list
 
-if __name__ == '__main__':
+#if __name__ == '__main__':
 
-    cars = []
-    trucks = []
-    spec_macs = []
+   # data = get_car_list('_af3947bf3a1ba3333b0c891e7a8536fc_coursera_week3_cars.csv')
+    #test = data[1]
+    #print(test.brand)
 
-    data = get_car_list('_af3947bf3a1ba3333b0c891e7a8536fc_coursera_week3_cars.csv')
-   # print(car_data)
-    for i in range(len(data) - 1):
-        if data[i][0] == "car":
-            car = Car(data[i][0], data[i][1], data[i][3], data[i][5], data[i][2])
-            cars.append(car)
-        elif data[i][0] == "truck":
-            truck = Truck(data[i][0], data[i][1], data[i][3], data[i][5], data[i][4])
-            trucks.append(truck)
-        elif data[i][0] == "spec_machine":
-            spec_mac = SpecMachine(data[i][0], data[i][1], data[i][3], data[i][5], data[i][6])
-            spec_macs.append(spec_mac)
-        else:
-            print("No such type:", data[i][0])
 
-        print("Cars:", cars)
-        print("Trucks:", trucks)
-        print("Spec_macs:", spec_macs)
-
-###   neeed debug, why not attribute car_type
-        print(car.car_type)
-        for car in range(len(cars)):
-            print("photo_car:", cars[car].get_photo_file_ext())
-
-            print("photo_car:", cars[car].get_photo_file_ext())
-
-        for truck in range(len(trucks)):
-            print('body_size_track:', trucks[truck].body_whl)
-            print("photo_truck:", trucks[truck].get_photo_file_ext())
