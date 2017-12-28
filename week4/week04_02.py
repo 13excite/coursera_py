9a1662996b71221ae34583f6417de09f77002451
Vladimir <originalblack@mail.ru>
2017-12-29 00:39:15 +0300

    done task and disabled tests

--- week4/week04_02.py
+++ week4/week04_02.py
@@ -3,7 +3,7 @@ class Value():
     def __init__(self, amount=None):
         self.amount = amount
 
-    def __get__(self, instance):
+    def __get__(self, instance, val):
         return self.amount
 
     def __set__(self, instance, value):
@@ -16,7 +16,7 @@ class Account:
     def __init__(self, commission):
         self.commission = commission
 
-new_account = Account(0.1)
-new_account.amount = 100
+#new_account = Account(0.1)
+#new_account.amount = 100
 
-print(new_account.amount)
+#print(new_account.amount)
