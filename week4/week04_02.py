7b19f7eded32605f4a7c73d17b8dfce07981080a
Vladimir <originalblack@mail.ru>
2017-12-29 00:30:07 +0300

    add tests

--- week4/week04_02.py
+++ week4/week04_02.py
@@ -3,11 +3,11 @@ class Value():
     def __init__(self, amount=None):
         self.amount = amount
 
-    def __get__(self):
+    def __get__(self, instance):
         return self.amount
 
     def __set__(self, instance, value):
-        self.amount = value - value * instance.commisson
+        self.amount = value - value * instance.commission
 
 
 class Account:
@@ -16,3 +16,7 @@ class Account:
     def __init__(self, commission):
         self.commission = commission
 
+new_account = Account(0.1)
+new_account.amount = 100
+
+print(new_account.amount)
