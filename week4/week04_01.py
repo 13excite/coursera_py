9bfde8871e7e1efebf382e3b005c09901d945bd7
Vladimir <originalblack@mail.ru>
2017-12-28 22:48:18 +0300

    add skeleteon for task4_01

---
+++ week4/week04_01.py
@@ -0,0 +1,20 @@
+import os
+import tempfile
+
+
+class File():
+
+    def __init__(self, filename):
+        self.filename = filename
+
+    def __add__(self, other):
+        pass
+
+    def __getitem__(self, item):
+        pass
+
+    def __repr__(self):
+        pass
+
+    def write(self, string):
+        pass
