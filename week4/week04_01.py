8e228a1332791d83800605f99e7404c3208b401d
Vladimir <originalblack@mail.ru>
2017-12-28 23:14:37 +0300

    add tests

--- week4/week04_01.py
+++ week4/week04_01.py
@@ -20,3 +20,17 @@ class File():
 
     def write(self, string):
         pass
+
+obj = File('/tmp/file.txt')
+
+obj.write('line\n')
+
+first = File('/tmp/first')
+second = File('/tmp/second')
+
+new_obj = first + second
+print(new_obj)
+
+obj_test = File('/tmp/file.txt')
+
+print(obj_test)
