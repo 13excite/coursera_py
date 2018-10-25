92d482aec2732866027600dbb792013c227576c7
Vladimir Dulenov <originalblack@mail.ru>
2018-10-25 18:57:13 +0300

    done week5

--- week5/week05_01.py
+++ week5/week05_01.py
@@ -1,50 +1,76 @@
 import socket
 import time
-import collections as cllns
 
 # class for client obj put exception
 class ClientError(socket.error):
     pass
 
 class Client:
+    def __init__(self, addr, port, timeout=None):
+        self.addr = addr
+        self.port = port
+        try:
+            self.sock = socket.create_connection((self.addr, self.port), timeout)
+        except socket.error as err:
+            ClientError("error create connection", err)
 
-    def __init__(self, host, port, timeout=None):
-        self._host = host
-        self._port = port
-        self._timeout = timeout
+    def _read(self):
+        data = b""
 
-    def put(self, name, value, timestamp=None):
-        timestamp = timestamp or int(time.time())
-        with socket.create_connection((self._host, self._port), self._timeout) as s:
-            data = 'put %s %f %d\n' % (name, value, timestamp)
+        while not data.endswith(b"\n\n"):
             try:
-                s.send(data.encode('utf-8'))
-            except socket.error:
-                raise ClientError
+                data += self.sock.recv(1024)
+            except socket.error as err:
+                raise ClientError("error recv data", err)
+        
+        decoded_data = data.decode()
 
-    def get(self, key_metric):
-        data_dict = cllns.defaultdict(list)
-        with socket.create_connection((self._host, self._port), self._timeout) as s:
-            msg = 'get %s\n' % key_metric
-            try:
-                s.send(msg.encode('utf-8'))
-            except socket.error:
-                raise ClientError
-            data = s.recv(1024).decode('utf-8')
-            data = [i.split() for i in data.split('\n')[1:] if len(i) > 1]
-            [data_dict[i[0]].append((int(i[2]), float(i[1]))) for i in data]
-        if key_metric == '*':
+        status, payload = decoded_data.split("\n", 1)
+        payload = payload.strip()
+
+        if status == "error":
+            raise ClientError(payload)
+        
+        return payload
+
+    def put(self, metric_name, metric_val, timestamp = int(time.time())):
+        try:
+            self.sock.sendall("put {} {} {}\n".format(metric_name, metric_val, timestamp).encode())
+        except socket.error as err:
+            raise ClientError("error send data", err)
+    
+        self._read()
+        
+    def get(self, metric_name):
+        try:
+            self.sock.sendall("get {}\n".format(metric_name).encode())
+        except socket.error as err:
+            raise ClientError("error send data")
+        payload = self._read()
+
+        data = {}
+        if payload == "":
             return data
-        else:
-            data = {key_metric: data.get(key_metric)}
-            return data
+
+        for row in payload.split("\n"):
+            key, value, timestamp = row.split()
+            if key not in data:
+                data[key] = []
+            data[key].append((int(timestamp), float(value)))
+        return data
+    
+    def close(self):
+        try:
+            self.sock.close()
+        except socket.error as err:
+            raise ClientError("error close connection", err)
 
 
 
 #### tests
-client = Client("127.0.0.1", 8888, timeout=15)
+#client = Client("127.0.0.1", 8888, timeout=15)
 
-client.put("palm.cpu", 0.5, timestamp=1150864247)
+#client.put("palm.cpu", 0.5, timestamp=1150864247)
 #client.put("palm.cpu", 2.0, timestamp=1150864248)
 #client.put("palm.cpu", 0.5, timestamp=1150864248)
 
